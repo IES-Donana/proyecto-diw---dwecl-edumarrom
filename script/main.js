72d36e0b509b7e7dbaef5faa5f998725c39366e4
Edu Martínez <emartinezr161@outlook.com>
2021-12-03 17:19:01 +0100

    implementado formulario

---
+++ script/main.js
@@ -0,0 +1,6 @@
+const nombre = document.querySelector('#nombre');
+const email = document.forms[0].email;
+const asunto = document.querySelector('#asunto');
+const asunto = document.querySelector('#mensaje');
+
+console.log(email);
