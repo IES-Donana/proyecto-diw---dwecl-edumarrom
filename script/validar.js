80f474c845bdf00d1870cf3c13d7af33c1bfce64
Eduardo Martínez <emartinezr161@outlook.com>
2021-12-08 21:54:19 +0100

    formulario funcional. pendientes retoques

--- script/validar.js
+++ script/validar.js
@@ -1,11 +1,36 @@
 const boton = document.getElementById('enviar');
 boton.addEventListener('click', validar);
 
+// Crea el elemento que mostrará el error
+const mensajeError = document.createElement('div');
+mensajeError.classList.add('entrada-error-mensaje');
+
+// Mensajes de error
+const missing = (e) =>`El ${e.name} no puede estar vacío.`;
+const missmatch = (e) =>`Debe introducir un ${e.name} válido.`;
+const tooshort = (e) => `El ${e.name} debe tener al menos ${e.minLength} caracteres.`;
+const toolong = (e) => `El ${e.name} no puede superar los ${e.minLength} caracteres.`;
+
+function error(e) {
+  mensajeError.textContent = e.validationMessage;
+  e.classList.add('entrada-error');
+  e.insertAdjacentElement("afterend", mensajeError);
+}
+
+function borrarError() {
+  const formulario = document.forms[0];
+  for (const e of formulario.elements){
+    e.classList.remove('entrada-error');
+    e.setCustomValidity('');  //Si no ponemos el mensaje en blanco, se determinará validación incorrecta
+    mensajeError.textContent = e.validationMessage;
+  }
+}
+
 function validar(evento) {
   borrarError();
-  if(validaNombre() && validaTelefono() && validaFechaNac() && validaCheck()
+  if(validaNombre() && validaEmail() && validaAsunto() && validaMensaje()
     && confirm("¿Confirmar envío de formulario?")) {
-      return true;
+    return true;
   } else {
     evento.preventDefault();
     return false;
@@ -17,11 +42,10 @@ function validaNombre() {
   const e = document.querySelector('#nombre');
   if(!e.checkValidity()) {
     if(e.validity.valueMissing) {
-        e.setCustomValidity('Debe introducir un nombre.');
+        e.setCustomValidity(missing(e));
     }
     if(e.validity.patternMismatch) {
-        e.setCustomValidity(`Debe introducir un nombre que no contenga números,
-        empiece por una letra y no supere los 45 caracteres.`);
+        e.setCustomValidity(missmatch(e));
     }
     e.reportValidity();
     error(e);
@@ -31,64 +55,60 @@ function validaNombre() {
   return true;
 }
 
-function validaTelefono() {
+function validaEmail() {
   borrarError();
-  const e = document.querySelector('#telefono');
+  const e = document.querySelector('#email');
   if(!e.checkValidity()) {
     if(e.validity.valueMissing) {
-      e.setCustomValidity('Debe introducir un teléfono.')
+        e.setCustomValidity(missing(e));
     }
     if(e.validity.patternMismatch) {
-      e.setCustomValidity('El teléfono debe contener 9 cifras.');
+        e.setCustomValidity(missmatch(e));
     }
     e.reportValidity();
     error(e);
     return false;
   }
-  console.log('Teléfono correcto');
+  console.log('Email correcto');
   return true;
 }
 
-function validaFechaNac() {
+function validaAsunto() {
   borrarError();
-  const e = document.querySelector('#fechaNac');
+  const e = document.querySelector('#asunto');
   if(!e.checkValidity()) {
     if(e.validity.valueMissing) {
-      e.setCustomValidity('Debe introducir una fecha.')
+        e.setCustomValidity(missing(e));
     }
-    if(e.validity.rangeUnderflow) {
-      e.setCustomValidity('La fecha no puede ser inferior al 01/01/1990.')
+    if(e.validity.patternMismatch) {
+        e.setCustomValidity(missmatch(e));
     }
     e.reportValidity();
     error(e);
     return false;
   }
-  console.log('Fecha correcta');
+  console.log('Asunto correcto');
   return true;
 }
 
-function validaCheck() {
+function validaMensaje() {
   borrarError();
-  const e = document.querySelector('#mayor');
+  const e = document.querySelector('#mensaje');
   if(!e.checkValidity()) {
-    e.setCustomValidity('Confirma que eres mayor de edad.')
+    if(e.validity.valueMissing) {
+        e.setCustomValidity(missing(e));
+    }
+    if(e.validity.tooShort) {
+        e.setCustomValidity(tooshort(e));
+    }
+
+    if(e.validity.tooLong) {
+      e.setCustomValidity(toolong(e));
+  }
+    e.reportValidity();
     error(e);
     return false;
   }
-  console.log('Mayor confirmado');
+  console.log('Mensaje correcto');
   return true;
-}
-
-function error(e) {
-  document.getElementById('mensajeError').innerHTML = e.validationMessage;
-  e.className = 'error';
-}
-
-function borrarError() {
-  const formulario = document.forms[0];
-  for (const e of formulario.elements){
-    e.className = '';
-    e.setCustomValidity('');  //Si no ponemos el mensaje en blanco, se determinará validación incorrecta
-    document.getElementById('mensajeError').innerHTML = e.validationMessage;
-  }
 }
